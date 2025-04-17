c7313cc988a87e81d42b7619a86114d5779eaa9e
alexandre-anabuki <alex.anabuki@gmail.com>
2025-04-16 22:16:40 -0300

    props teste

---
+++ props/src/components/Produto.jsx
@@ -0,0 +1,12 @@
+import './Porduto.css'
+
+function Produto(props) {
+  return (
+    <div className='container-produto'>
+        <h2>{props.modelo}</h2>
+        <p>R$:{props.preco.toFixed(2).replace(".", ",")}</p>
+    </div>
+  )
+}
+
+export default Produto
