7966eb8e3e017b98fb4c08b52d11c1b10d1f4587
alexandre-anabuki <alex.anabuki@gmail.com>
2025-04-23 22:26:40 -0300

    Programação de aplicativos - atividades

---
+++ farmacia/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container-app'>
+      <div className='cabecalho'>
+        <div className='logo'>logo</div>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
+        <div className='item'>4</div>
+
+      </div>
+
+      <div className='corpo'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
--- props/src/components/Produto.jsx
+++ props/src/components/Produto.jsx
@@ -4,7 +4,8 @@ function Produto(props) {
   return (
     <div className='container-produto'>
         <h2>{props.modelo}</h2>
-        <p>R$:{props.preco.toFixed(2).replace(".", ",")}</p>
+        <p>R${props.preco.toFixed(2).replace(".", ",")}</p>
+        <p>{props.key}</p>
     </div>
   )
 }
